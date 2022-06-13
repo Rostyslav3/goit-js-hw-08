f027f8b32ea097fc3f052089a13af65268146158
Rostyslav Marchenko <marchenko.r.7@gmail.com>
2022-06-14 01:00:46 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const feedbackFormRef = document.querySelector('.feedback-form');
+const emailRef = feedbackFormRef.elements.email;
+const messageRef = feedbackFormRef.elements.message;
+const formData = {};
+const keyMesage = 'message';
+
+feedbackFormRef.addEventListener('input', throttle(onFormInput, 500));
+
+feedbackFormRef.addEventListener('submit', onFormSubmit);
+
+getLocaleStorageData();
+
+setformData();
+
+function getLocaleStorageData() {
+  if (localStorage.getItem(keyMesage)) {
+    const storageData = JSON.parse(localStorage.getItem(keyMesage));
+    emailRef.value = storageData.email;
+    messageRef.value = storageData.message;
+  }
+}
+
+function setformData() {
+  formData.email = emailRef.value;
+  formData.message = messageRef.value;
+}
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(keyMesage, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+    if (formData.email !== '' && formData.message !== '') {
+        console.log(formData);
+
+        e.currentTarget.reset();
+        localStorage.removeItem(keyMesage);
+        setformData();
+    }
+}
